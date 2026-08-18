bcff53e5f93cd9798b642b25b7909774982f12ba
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-08-19 01:52:18 +0530

    Load immersive world V3 styling last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import './experience.css';
 import './experience-lock-v2.css';
 import './motion.css';
+import './experience-world-v3.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
