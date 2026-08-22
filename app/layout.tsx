13e9606166b65ba614e51c973c238d2c2261ce5c
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-08-23 03:20:37 +0530

    style: load dedicated mobile navigation styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./mobile.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 
