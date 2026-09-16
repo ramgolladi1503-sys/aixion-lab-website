14d4e5c518f244539953706d6501974c725a6a0a
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-09-17 01:21:27 +0530

    style: load editorial flow after art-wall layers

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./page-backgrounds.css";
 import "./wallpaper-continuity-fix.css";
 import "./art-wall-final.css";
 import "./art-wall-polish.css";
+import "./editorial-flow.css";
 import { UnseenHeader, UnseenStatusBar } from "@/components/unseen-chrome";
 
 const aixionDisplay = Instrument_Serif({
